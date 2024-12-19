0ac15f92105c26738d23c01d8a1457c324a84ac2
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-12-19 18:26:27 -0300

    dando segundo deploy

--- client/desktop/vite.config.js
+++ client/desktop/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pmv-ads-2024-2-e4-proj-dad-t3-autocare/',
+  base: '/autocare/',
   plugins: [react()],
   test: {
     globals: true,
